77dee1b3fad5ffe832b125bb1d4896525b0cb9de
MANISH7830 <ms2500287@gmail.com>
2026-05-01 15:14:39 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,17 @@
+n1=20
+n2=30
+
+#swap logic
+temp=n1
+n1=n2
+n2=temp
+print(f"n1={n1}  n2={n2}")
+
+char1="hello"
+char2="java"
+
+#swap logic
+temp1=char1
+char1=char2
+char2=temp1
+print(f"char1={char1}  char2={char2}")
