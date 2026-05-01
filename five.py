14bfaa038000355b7e4beb2b49e8fb49c4a81a0b
MANISH7830 <github>
2026-05-01 23:05:16 +0530

    first commit

--- five.py
+++ five.py
@@ -4,3 +4,10 @@ print(type(num1),type(num2))
 res=str(num1)+str(num2)
 print(type(res))
 
+
+n1=200
+char2="java"
+tem=n1
+n1=char2
+char2=tem
+print(f"n1={n1} , char2={char2}")
--- second.py
+++ second.py
@@ -1,14 +1,25 @@
-a=10.5
-b=15.5
-c=20.5
-d=25.5
-e=30.5
-f=35.5
-g=a+b+c+d+e+f
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
-print(g)
+num1=20
+num2=30
+num3=40
+num4=10
+res=num1+num2+num3+num4
+print(num1,"+",num2,"+",num3,"+",num4,"=",res)
+print(f"{num1}+{+num3}+{num4}")
+
+
+
+n1=20
+n2=30
+#swipe logic
+tem1=n1
+n1=n2
+n2=tem1
+print(f"n1={n1}  ,  n2={n2}")
+
+
+char1="hello"
+char2="java"
+tem2= char1
+char1=char2
+char2=tem2
+print(f"char1={char1}  ,  char2={char2}")
