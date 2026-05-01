c673a51250f780617fbaeff1e8498f556902b13e
MANISH7830 <ms2500287@gmail.com>
2026-05-01 13:06:04 +0530

    first commit

---
+++ third.py
@@ -0,0 +1,13 @@
+num1=int(input("enter your number:"))
+num2=int(input("enter your number:"))
+num3=int(input("enter your number:"))
+res=num1-num2-num3
+print(res)
+res1=num1*num2*num3
+print(res1)
+res2=num1+num2+num3
+print(res2)
+res3=num1//num2//num3
+print(res3)
+res4=num1**num2
+print(res4)
