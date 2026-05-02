69ea20dbdff8b629a13c99f3eb3aec565f795eb8
MANISH7830 <github>
2026-05-02 13:21:49 +0530

    first commit

--- four.py
+++ four.py
@@ -8,3 +8,24 @@ print(type(var2))
 var3=1000
 print(type(var3))
 print(var3)
+
+
+before=12000
+print(before)
+w= 3000
+after=  before - w
+print("12000-3000",after)
+
+be=12000
+wi=3000
+after=be-wi
+print(f"be{be}-wi{wi}=after{after}")
+
+
+inc=3
+beforecartit=5
+after1=beforecartit+inc
+print(f"inc{inc}+beforecartit{beforecartit}=after1{after1}")
+print(type(inc),type(beforecartit))
+
+
---
+++ free.py
@@ -0,0 +1,19 @@
+# Type casting
+
+var1=float(1000)
+print(type(var1))
+print(var1)
+
+num1="50"
+num2="20"
+res=int(num1)+int(num2)
+print(res)
+print(type(res))
+print(type(num1))
+
+num1=50
+num2=20
+print(type(num1),type(num2))
+res=str(num1)+str(num2)
+print(res)
+print(type(res))
