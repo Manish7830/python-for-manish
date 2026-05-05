5ee0cc3cff9415aa32a4e85cdc3777f0a303c80f
MANISH7830 <github>
2026-05-05 09:39:14 +0530

    first commit

--- day1.py
+++ day1.py
@@ -1,1 +1,43 @@
-print("hello world")
+# Arithmetic Operators
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#Assignment Operators
+x = 5
+
+print(x)
+
+# Comparison Operators
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Logical Operators
+x = 5
+
+print(x > 0 and x < 10)
+
+# Identity Operators
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
