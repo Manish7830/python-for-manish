0ac8fc9a7ae5b2f5d72da4fa2fb07e973d8248f8
MANISH7830 <github>
2026-05-05 09:32:13 +0530

    first commit

--- Assignment-1.py
+++ Assignment-1.py
@@ -6,10 +6,18 @@ n1=n2
 n2=temp
 print(f"n1={n1},n2={n2}")
 
-
+#2. Switch values of two strings (characters) 
 char1="hello"
 char2="java"
 temp1=char1
 char1=char2
 char2=temp1
 print(f"char1={char1},char2={char2}")
+
+#Switch one string value with one integer value 
+#Input:
+n3=200
+char3="java"
+temp2=n3
+n3=char3
+print
--- free.py
+++ free.py
@@ -1,19 +1,8 @@
-# Type casting
+a=20
+a+=30
+print(a)
+a*=20
+print(a)
 
-var1=float(1000)
-print(type(var1))
-print(var1)
 
-num1="50"
-num2="20"
-res=int(num1)+int(num2)
-print(res)
-print(type(res))
-print(type(num1))
 
-num1=50
-num2=20
-print(type(num1),type(num2))
-res=str(num1)+str(num2)
-print(res)
-print(type(res))
