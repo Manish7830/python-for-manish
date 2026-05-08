cf1eda4e31fa63764cca0b39c55224466fa48375
MANISH7830 <github>
2026-05-08 10:24:29 +0530

    Practice

---
+++ may/8-may/class8.py
@@ -0,0 +1,65 @@
+#loops in python 
+# 1.for loops : ranged based
+# 2.while loops : condition
+
+
+
+# 1. for loops
+# range (start:0,stop -1 , step:1)
+
+# for i in range(5,10,1):
+#     print(i)
+
+# for i in range(5,10,1):
+#     print(i,end="")
+
+# for i in range(5,10,1):
+#     print(i,end=" ")
+
+
+# for i in range(1,20,1):
+#     print(i)
+#     print("manish")
+
+# for i in range(1,20,1):
+#     if i == 10:
+#         break
+#     print(i)
+
+# for i in range(20):
+#     if i == 10:
+#         continue
+#     print(i,end=" ")/
+
+# for i in range(21):
+#     if i%2==0:
+#         print(i,end=" ")
+
+# for i in range(21):
+#     if i%2 != 0:
+#         print(i,end=" ")
+
+# for i in range(21):
+#     if i%2 != 0:
+#         print(f"odd={i}")
+#     else :
+#         print(f"even={i}")
+
+
+
+# s=0
+# for i in range(1,5):
+#     s=s+i
+#     print(s)
+
+
+
+# s=1
+# for i in range(1,5):
+#     s=s*i
+#     print(s)
+
+# for i in range(10,1,-1):
+#     print(i)
+# for i in range(10,-5,-1):
+#     print(i)
