75b43524730892b5fb43352a453aa2cf4cee70cd
MANISH7830 <github>
2026-05-08 11:04:12 +0530

    Practice

---
+++ may/8-may/free.py
@@ -0,0 +1,24 @@
+# start=int(input("enter your start point = "))
+# end =int(input("enter your end point = "))
+
+# for i in range(start,end):
+#    if i%2==0 and i%3==0:
+#      print(i)
+
+# start=int(input("enter your start point = "))
+# end =int(input("enter your end point = "))
+# for i in range(start,end):
+#     if i%2==0 or i%3==0:
+#         print(i)
+
+
+
+# start=int(input("enter your table number = "))
+# for i in range(1,11,1):
+#     d = start*i
+#     print(f"{start} x {i} = {d} ")
+
+# start = int(input("enter your table numer = "))
+# for i in range(11,0,-1):
+#     d = start*i
+#     print(f"{start} x {i} = {d}")
